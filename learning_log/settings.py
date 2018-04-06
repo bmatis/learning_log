a4a5d4c716c974f9677921a898647e66123d6498
bmatis <bmatis@mac.com>
2018-04-06 10:49:09 -0700

    Moved secret key to environment variable so it won't get to GitHub.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v4w^8r&1yfir7x*1_j@_into^t&&f2oh13hwee76wlg1!*o$_g'
+# Get this value from environment variable so it's not commited to version control.
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
